0f6737d7b513176d5481a844e12cac4b3d16ebb4
Wei-Hao Wang <wwang03@risd.edu>
2020-02-27 12:44:47 -0500

    text marquee

--- index.php
+++ index.php
@@ -12,32 +12,25 @@ require_once('./views/jobs.php');
 require_once('./views/permitted_event.php');
 // assemble $msg
 $msg  = 'New York Consolidated . . . ';
-$msg .= $now;
-$msg .= ' currently ' . $output['wind_string'];
-$msg .= ' /// Currently ' . $output['temp_f'] . ' degrees.';
-$msg .= ' There are trains arriving at: ' . $output_train.".";
-$msg .= ' ' . $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".";
-$msg .= ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".";
+// $msg .= $now;
+// $msg .= ' currently ' . $output['wind_string'];
+// $msg .= ' /// Currently ' . $output['temp_f'] . ' degrees.';
+// $msg .= ' There are trains arriving at: ' . $output_train.".";
+// $msg .= ' ' . $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".";
+// $msg .= ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".";
 $msg .= ' 0 1 2 3 4 5 6 7 8 9 Have a nice day.';
 
-
-// *** below is for testing ***
-
-// $msg = $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".";
-
-// $msg = $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".";
 ?>
 
 <div id="mask">
-    <div id="scroller"></div>   
+    <div id="scroller">
+    </div>   
 </div>   
     
-<script src="./static/js/scroller.js"></script>
+<script src="./static/js/marquee.js"></script>
 <script>        
-	// Wei: the third parameter is added to control speed. It's time interval so the smaller the faster.
-    Scroller.init('scroller', 80,7, 30);
-    Scroller.enqueue('<?= $msg; ?>');
-    Scroller.start();
+	
+    Text_marquee('scroller','<?= $msg; ?>',385);
 </script>
 
 </html>
