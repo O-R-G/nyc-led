c4ca032543dccc5458636a95dbbd8799031ec979
Wei-Hao Wang <wwang03@risd.edu>
2020-02-27 12:52:23 -0500

    cancel some comments which I made when trying thing out

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('./views/head.php');
 
 // get time
 date_default_timezone_set("America/New_York");
-// $now = date("h:i:sa");
+$now = date("h:i:sa");
     
 // get temperature
 require_once('./views/temp.php');
@@ -12,12 +12,12 @@ require_once('./views/jobs.php');
 require_once('./views/permitted_event.php');
 // assemble $msg
 $msg  = 'New York Consolidated . . . ';
-// $msg .= $now;
-// $msg .= ' currently ' . $output['wind_string'];
-// $msg .= ' /// Currently ' . $output['temp_f'] . ' degrees.';
-// $msg .= ' There are trains arriving at: ' . $output_train.".";
-// $msg .= ' ' . $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".";
-// $msg .= ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".";
+$msg .= $now;
+$msg .= ' currently ' . $output['wind_string'];
+$msg .= ' /// Currently ' . $output['temp_f'] . ' degrees.';
+$msg .= ' There are trains arriving at: ' . $output_train.".";
+$msg .= ' ' . $output_jobs["job_agency"] . ' is hiring ' . $output_jobs["job_title"] . " at " . $output_jobs["job_division"] . ", " . $output_jobs["job_location"] . ".";
+$msg .= ' ' . $output_permitted_event["event_name"] . ' will be happening from ' . $output_permitted_event["event_start_time"] . " until " . $output_permitted_event["event_end_time"] . ", at " . $output_permitted_event["event_location"] . ".";
 $msg .= ' 0 1 2 3 4 5 6 7 8 9 Have a nice day.';
 
 ?>
