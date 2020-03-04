cf58c22fe8a572c3de92f504be41247736df9a1d
david reinfurt <reinfurt@o-r-g.com>
2020-03-04 11:58:28 -0500

    add static/js/matrix.js and adjust css

---
+++ views/matrix.php
@@ -0,0 +1,14 @@
+<style>
+* {margin: 0; padding: 0;}
+body {background: white;}
+canvas {
+    display: block;
+    margin-top: 20%;
+    margin-left: 20%;
+}
+</style>
+
+<canvas id="c"></canvas>
+
+<script src='/static/js/matrix.js'></script>
+
