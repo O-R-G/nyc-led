084ac48a1c3dba21372f4326dbc282e9f8ad8725
david reinfurt <reinfurt@o-r-g.com>
2020-03-18 15:19:41 -0400

    add views/menu

---
+++ views/menu.php
@@ -0,0 +1,17 @@
+<style>
+#menu {
+    position: absolute;
+    top: 10px;
+    left: 300px;
+    width: 75%;
+    font-size: 36px;
+    background: #FFF;
+}
+</style>
+
+<div id="menu">
+<p>New York Consolidated is a new nonprofit organization founded to leverage accountable, trust-based philanthropy to support equity in the arts. We stage exhibitions, publish books, fund research, provide a public space for study and conversation and advocate on behalf of the diverse artists (and platforms?) that are integral to the health and vibrancy of our society.</p>
+<br>
+<p>We envision a future where art’s value is determined by its cultural and social impact and where all artists benefit from resources and visibility. We achieve this vision by producing exhibitions, publishing books, funding research, engaging in advocacy and offering a public space for study and conversation. We will shine a light on artists whose work demands our attention.</p>
+</div>
+    
