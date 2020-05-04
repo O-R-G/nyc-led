d6ba17f99c36cb6fd1de774dd252bf6e1f0ffd70
Wei-Hao Wang <wwang03@risd.edu>
2020-05-04 17:50:15 -0400

    test mta

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 $request = $_SERVER['REQUEST_URI'];
 $requestclean = strtok($request,"?");
 $uri = explode('/', $requestclean);
