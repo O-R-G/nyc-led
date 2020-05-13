0cd719c9b4660ffc5947ff3004218d9304e48634
david reinfurt <reinfurt@o-r-g.com>
2020-05-12 22:05:29 -0400

    debug collapsing white-space issue in matrix.js

--- static/js/matrix.js
+++ static/js/matrix.js
@@ -14,7 +14,7 @@
     from msgs.js:
     msg[]       text to be shown
 
-    currently triggered from static/js/json.js:
+    triggered static/js/json.js:
     var timer = setInterval(update, timer_ms);
 */
 
@@ -22,24 +22,26 @@ var rows = query_rows;                  // [4]
 var columns = query_columns;            // [21]
 var font_size = query_font_size;        // [18] 24 36 48
 var font_leading = font_size * 1.1667;  // [21]
+var font_w_to_h = .605;                 // helveticaautospaced
+var font_letterspacing = 10;            // 5 [7] 10 20
 var font = query_font;
 var timer;                  // update
 var delay;                  // pause between messages
-var timer_ms = 30;          // ms before next update [30]
-var delay_ms = 6000;        // ms after msg complete
+// var timer_ms = 30;          // ms before next update [30]
+// var delay_ms = 6000;        // ms after msg complete
+var timer_ms = 5;          // ms before next update [30]
+var delay_ms = 10000;        // ms after msg complete
 var updates = 0;            // counter
 var updates_max = 50;       // times to try to match letter [50]
 var pointer = 0;
 
 var d = document.getElementById('d');
-// ** fix **  ----->
-// currently uses letter-spacing to accommodate (in main.css)
-// could use proportion of the letter to do the same thing
-// then possibly adjust letter-spacing after that
-d.style.width = font_size * columns + 'px';     // ** fix **
-d.style.height = font_leading * rows + 20 + 'px';  // ** fix **
 d.style.fontSize = font_size + 'px'; 
-d.innerHTML = '••••••••••••••••••'; 
+d.style.letterSpacing = font_letterspacing + 'px'; 
+d.style.width = (font_w_to_h * font_size * columns) +
+                (font_letterspacing * columns) + 'px';
+d.style.height = font_leading * rows + 'px';
+// d.style.backgroundColor = '#00F';
 d.onclick = stop_start;
 
 var mask = document.getElementById('mask');
@@ -49,10 +51,11 @@ mask.style.width = d.style.width;
 var isBeginning = true;     
 
 function update() {
-    // init * should this be moved? *
-    // isBeginning is the problem, likely to do with variable closure
-    // in setInterval calls
-    console.log(isBeginning);
+    
+// -----> ** fix **
+// init * should this be moved? *    
+// isBeginning in setInterval calls -- variable closure?
+// console.log(isBeginning);
 
     if(isBeginning){
         update_msgs_opening();
@@ -63,22 +66,6 @@ function update() {
         isBeginning = false;
     }
 
-// html swallows the repeated spaces
-// even though css white-space: pre-wrap
-// so replace every ' ' with \xa0
-// \xa0 == non-breaking space (&nbsp)
-// ugly way for now, before reworking 
-// update_msgs() and update_msgs_opening()
-
-// now does not match on \xa0 when comparing to ' ' below
-// damn!
-
-// msg_str = msg.join('').replace(/ /g, '\xa0');
-msg_str = msg.join('').replace(/ /g, '•');
-msg = msg_str.split('');
-// console.log(msg_str);
-// console.log(msg);
-
     // display, compare to random letter
     var i;          
     for (var y = 0; y < rows; y++) {
@@ -87,29 +74,14 @@ msg = msg_str.split('');
             if ((letters[i] !== msg[i]) && (updates <= updates_max)) {
                 letters[i] = msgs[Math.floor(Math.random()*msgs.length)];   // one random char
             } else {
-                // letters[i] = msg[i];                
-                letters[i] = i % 10;
-
-// ** debug **
-// so this above is not counting the right numbers when working through ... 
-// actually just not drawing the correct number of columns
-// which was juryrigged above using letterspacing so also need to sort that
-// maybe is ignoring spaces?
-// these are the consistent positions (see above)
-// first column of each row
-    
-// if ((i == 21) || (i == 42) || (i == 63))
-//     console.log('---->' + i + ' : ' + letters[i]);
-
+                letters[i] = msg[i];
                 if(typeof letters[i] == 'undefined')
                     letters[i] = '•';
             }
         }
     }
 
-    // check / compare speed ----> ** fix **
-    // d.innerText = letters.join('');
-    d.innerHTML = letters.join('');
+    d.innerText = letters.join('');
 
     // all letters resolved or timed out, move to next msg
     if (letters.join('') == msg.join('')) {
@@ -117,10 +89,21 @@ msg = msg_str.split('');
         delay = setInterval(stop_start, delay_ms);
         letters = [];
         pointer += msg.length;
-        if (pointer >= msgs.length){
+console.log('msgs.length : ' + msgs.length);
+console.log('pointer : ' + pointer);
+        // if (pointer >= msgs.length){
+// ----> ** fix **
+// hack !
+// somehow there are extra characters in msgs beyond this count
+// maybe because does not add chars when fills out empty spaces?
+// not sure
+        if (pointer >= msgs.length - 100){
             pointer = 0;
+
+// ----> ** fix **
 // never gets here on the last one ... 
-// may have to do with collapsing whitespace and removing blanks at end-of-line
+console.log('finished');
+
             isBeginning = true;
             call_request_json();
         }
@@ -128,7 +111,6 @@ msg = msg_str.split('');
         msg = msgs.join('').substr(pointer,columns*rows).split('');
         timer = false;
         updates = 0;
-
     } else
         updates++;
 }
