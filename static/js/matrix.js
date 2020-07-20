77b571770f07341598bf64fe809999e0b03618b3
Wei-Hao Wang <wwang03@risd.edu>
2020-07-20 11:08:07 -0400

    figure the mobile issue

--- static/js/matrix.js
+++ static/js/matrix.js
@@ -25,7 +25,7 @@ var rows;
 var columns;
 var font = query_font;
 var font_size = query_font_size;        // [18] 24 36 48
-if(window.innerWidth < 500)
+if(window.innerWidth < 768)
     font_size = 22;
 var font_leading = font_size * 1.1667;  // [21]
 var font_w_to_h = .605;                 // helveticaautospaced
